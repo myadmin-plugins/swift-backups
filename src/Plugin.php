799193d62e105b1cc9a0a21de66a53d5fd36bdec
Joe Huss <detain@interserver.net>
2018-11-13 20:32:21 -0500

    localization updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -44,7 +44,7 @@ class Plugin
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
 			if (has_acl('client_billing')) {
-				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', 'Swift');
+				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', __('Swift'));
 			}
 		}
 	}
@@ -54,19 +54,25 @@ class Plugin
 	 */
 	public static function getRequirements(GenericEvent $event)
 	{
-		$loader = $event->getSubject();
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
 		$loader->add_requirement('class.Swift', '/../vendor/detain/myadmin-swift-backups/src/Swift.php');
 	}
 
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		$settings = $event->getSubject();
-		$settings->add_text_setting('Backups', 'Swift', 'swift_auth_url', 'Swift Auth URL:', 'Swift Auth URL', SWIFT_AUTH_URL);
-		$settings->add_text_setting('Backups', 'Swift', 'swift_auth_v1_url', 'Swift Auth v1 URL:', 'Swift Auth v1 URL', SWIFT_AUTH_V1_URL);
-		$settings->add_text_setting('Backups', 'Swift', 'swift_admin_user', 'Swift Admin User:', 'Swift Admin User', SWIFT_ADMIN_USER);
-		$settings->add_text_setting('Backups', 'Swift', 'swift_admin_key', 'Swift Admin Key:', 'Swift Admin Key', SWIFT_ADMIN_KEY);
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+		$settings->add_text_setting(__('Backups'), __('Swift'), 'swift_auth_url', __('Swift Auth URL'), __('Swift Auth URL'), SWIFT_AUTH_URL);
+		$settings->add_text_setting(__('Backups'), __('Swift'), 'swift_auth_v1_url', __('Swift Auth v1 URL'), __('Swift Auth v1 URL'), SWIFT_AUTH_V1_URL);
+		$settings->add_text_setting(__('Backups'), __('Swift'), 'swift_admin_user', __('Swift Admin User'), __('Swift Admin User'), SWIFT_ADMIN_USER);
+		$settings->add_text_setting(__('Backups'), __('Swift'), 'swift_admin_key', __('Swift Admin Key'), __('Swift Admin Key'), SWIFT_ADMIN_KEY);
 	}
 }
