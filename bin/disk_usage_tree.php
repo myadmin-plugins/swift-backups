d1f9d445205f93584329471a57d0c8dfeea1c3fd
Joe Huss <detain@interserver.net>
2019-01-14 14:11:32 -0500

    added additional output aprameter handling verbosity control totals and more

--- bin/disk_usage_tree.php
+++ bin/disk_usage_tree.php
@@ -5,7 +5,7 @@ $verbose = 0;
 if ($_SERVER['argc'] == 1)
 	$verbose = -1;
 for ($x = 1; $x < $_SERVER['argc']; $x++) {
-	if (in_array($_SERVER['argv'][$x], ['repo','container','dir']))
+	if (in_array($_SERVER['argv'][$x], ['repo','container','file']))
 		$detail = $_SERVER['argv'][$x];
 	elseif (in_array($_SERVER['argv'][$x], ['-v', '-vv', '-vvv', '-vvvv']))
 		$verbose += strlen($_SERVER['argv'][$x]) - 1;
@@ -13,7 +13,7 @@ for ($x = 1; $x < $_SERVER['argc']; $x++) {
 		$verbose = -1;
 }
 if ($verbose == -1)
-	die("Syntax {$_SERVER['argv'][0]} [-v] <detail level>\n	<detail level> can be any of: repo, container, or dir\n	[-v] increase verbosity level (can be repeated)");	
+	die("Syntax {$_SERVER['argv'][0]} [-v] <detail level>\n	<detail level> can be any of: repo, container, or file\n	[-v] increase verbosity level (can be repeated)");	
 function_requirements('class.Swift');
 $sw = new Swift;
 $module = 'vps';
@@ -56,7 +56,9 @@ foreach ($json['accounts'] as $account_idx => $account_data) {
 		echo PHP_EOL;
 }
 $backups = [];
-$total = 0;
+$repo_total = 0;
+$container_total = 0;
+$file_total = 0;
 foreach ($repos as $repo) {
 	if ($verbose >= 1)
 		echo "Processing repo {$repo['name']}\n";
@@ -67,17 +69,19 @@ foreach ($repos as $repo) {
 	$usage = $sw->usage();
 	$repo_backup['value'] = (int)$usage['X-Account-Bytes-Used'];
 	echo "{$repo['name']} Got ".Scale($repo_backup['value'])." in backups\n";
-	$total = bcadd($repo_backup['value'], $total);
+	$repo_total = bcadd($repo_backup['value'], $repo_total);
 	if ($detail != 'repo') {
 		$ls_output = explode("\n", trim($sw->ls()));
 		foreach ($ls_output as $container) {
-			$container_backup = $detail != 'row' ? $new_dir : $new_file;
+			$container_backup = $detail != 'file' ? $new_dir : $new_file;
 			$container_backup['name'] = $container;
-			echo "Processing {$repo['name']} container '$container'\n";
+			if ($verbose >= 2)
+				echo "Processing {$repo['name']} container '$container'\n";
 			$container_backup['path'] = $repo['name'].'/'.$container;
 			$usage = $sw->usage($container);
 			$container_backup['value'] = (int)$usage['X-Container-Bytes-Used'];
-			if ($detail == 'row') {
+			$container_total = bcadd($container_backup['value'], $container_total);
+			if ($detail == 'file') {
 				$ls_vps_output = explode("\n", trim($sw->ls($container)));
 				foreach ($ls_vps_output as $filedir) {
 					if (preg_match('/\//', $filedir)) {
@@ -87,6 +91,7 @@ foreach ($repos as $repo) {
 						$file_backup['path'] = $repo['name'].'/'.$container.'/'.$filedir;
 						$usage = $sw->usage($container.'/'.$filedir);
 						$file_backup['value'] = (int)$usage['Content-Length'];
+						$file_total = bcadd($file_backup['value'], $file_total);
 						$container_backup['children'][] = $file_backup;
 					}
 				}
@@ -97,5 +102,9 @@ foreach ($repos as $repo) {
 	$backups[] = $repo_backup;
 }
 file_put_contents(__DIR__.'/../../../../public_html/admin/swift_usage.json', str_replace("\\/", '/', json_encode($backups, JSON_PRETTY_PRINT)));
-echo "Got Total Size ".Scale($total)."\n";
+echo "Got Total Size ".Scale($repo_total)."\n";
+if ($detail != 'repo')
+	echo "Got Total Containers Size ".Scale($container_total)."\n";
+if ($detail == 'file')
+	echo "Got Total Files Size ".Scale($file_total)."\n";
 echo "Wrote swift_usage.json\n";
